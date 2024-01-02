e43f62aa760de94f2c8ab1281988d2e6960ac1f6
JBreckeen <86539171+JBreckeen@users.noreply.github.com>
2024-01-02 12:25:50 -0500

    Added comments

--- ExportToDir/Scripts/ExportToDir.py
+++ ExportToDir/Scripts/ExportToDir.py
@@ -1,3 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+####################################################
+#
+# PRISM - Pipeline for animation and VFX projects
+#
+# www.prism-pipeline.com
+#
+# contact: contact@prism-pipeline.com
+#
+####################################################
+#
+#
+# Copyright (C) 2016-2021 Richard Frangenberg
+#
+# Licensed under GNU LGPL-3.0-or-later
+#
+# This file is part of Prism.
+#
+# Prism is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Prism is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Prism.  If not, see <https://www.gnu.org/licenses/>.
+####################################################
+#
+#           ExportToDir Plugin for Prism2
+#
+#                 Joshua Breckeen
+#                    Alta Arts
+#                josh@alta-arts.com
+#
+####################################################
+
+
+
 try:
     from PySide2.QtCore import *
     from PySide2.QtGui import *
