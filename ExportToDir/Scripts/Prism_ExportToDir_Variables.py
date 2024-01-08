49dc7f162f4fe91b614676dcffbdd65f9aa2e99d
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-01-07 20:15:07 -0500

    Up vervsion

--- ExportToDir/Scripts/Prism_ExportToDir_Variables.py
+++ ExportToDir/Scripts/Prism_ExportToDir_Variables.py
@@ -42,7 +42,7 @@
 
 class Prism_ExportToDir_Variables(object):
     def __init__(self, core, plugin):
-        self.version = "v2.0.2-1.3"
+        self.version = "v2.0.2-1.4"
         self.pluginName = "ExportToDir"
         self.pluginType = "Custom"
         self.platforms = ["Windows", "Linux", "Darwin"]
